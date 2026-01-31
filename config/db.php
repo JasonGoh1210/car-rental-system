18cc72bf8194f1b9b78c9fb773a52610c4fbb937
JasonGoh1210 <jason.goh.zong@student.mmu.edu.my>
2026-02-01 04:35:55 +0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,1 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db   = "car_rental_system";
 
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+}
+?>
